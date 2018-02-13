b55e3a91e69edf39312163c7ffcf50518fb60295
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-02-13 16:07:26 -0300

    Added user model

---
+++ model/user.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = Schema({
+    name: String,
+    surname: String,
+    nick: String,
+    email: String,
+    password: String,
+    role: String,
+    image: String
+});
+
+module.exports = mongoose.model('User', UserSchema);
