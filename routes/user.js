1050e42f0bf23f99bef821a1d50c7b3c65b0f621
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-02-27 03:02:12 -0300

    Authentication Middleware.

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,12 @@
 'use strict';
 
 var express = require('express');
-
 var UserController = require('../controllers/user');
-
 var api = express.Router();
+var md_auth = require('../middlewares/authenticated');
 
 api.get('/home', UserController.home);
-api.post('/test', UserController.test);
+api.post('/test', md_auth.ensureAuth, UserController.test);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 
