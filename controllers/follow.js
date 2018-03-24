bf9b69059003c5e3e71f711a1c25e2772cd78065
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-03-24 19:10:55 -0300

    Endpoint for delete a follow.

--- controllers/follow.js
+++ controllers/follow.js
@@ -13,13 +13,25 @@ function saveFollow(req, res) {
     follow.followed = params.followed;
 
     follow.save((err, followStored) => {
-        if (err) return res.status(500).send({message: "Saving follow request error."});
+        if (err) return res.status(500).send({message: "Saving follow error."});
         if (!followStored) return res.status(404).send({message: "User follow not saved."});
 
         return res.status(200).send({follow: followStored});
     });
 }
 
+function deleteFollow(req, res) {
+    var userId = req.user.sub;
+    var followId = req.params.id;
+
+    Follow.find({'user': userId, 'followed': followId}).remove(err => {
+        if (err) return res.status(500).send({message: "Deleting follow error."});
+
+        return res.status(200).send({message: 'Follow deleted.'});
+    });
+}
+
 module.exports = {
-    saveFollow
+    saveFollow,
+    deleteFollow
 };
--- routes/follow.js
+++ routes/follow.js
@@ -8,5 +8,6 @@ var md_auth = require('../middlewares/authenticated');
 //var md_upload = multipart({uploadDir: './uploads/users'});
 
 api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow);
+api.delete('/follow/:id', md_auth.ensureAuth, FollowController.deleteFollow);
 
 module.exports = api;
