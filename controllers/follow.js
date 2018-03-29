6786e371b285df686ad37f9f41d3af7bbb8697f7
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-03-29 16:35:40 -0300

    Endpoint for get followed users.

--- controllers/follow.js
+++ controllers/follow.js
@@ -59,8 +59,37 @@ function getFollowingUsers(req, res) {
     });
 }
 
+function getFollowedUser(req, res) {
+    var userId = req.user.sub;
+
+    if (req.params.id && req.params.page) {
+        userId = req.params.id;
+    }
+
+    var page = 1;
+    if (req.params.page) {
+        page = req.params.page;
+    } else {
+        page = req.params.id;
+    }
+
+    var itemsPerPage = 4;
+
+    Follow.find({followed: userId}).populate('user').paginate(page, itemsPerPage, (err, follows, total) => {
+        if (err) return res.status(500).send({message: "Get follow error."});
+        if (!follows) return res.status(404).send({message: "Without followers."});
+
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total/itemsPerPage),
+            follows: follows
+        });
+    });
+}
+
 module.exports = {
     saveFollow,
     deleteFollow,
-    getFollowingUsers
+    getFollowingUsers,
+    getFollowedUser
 };
--- routes/follow.js
+++ routes/follow.js
@@ -10,5 +10,6 @@ var md_auth = require('../middlewares/authenticated');
 api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow);
 api.delete('/follow/:id', md_auth.ensureAuth, FollowController.deleteFollow);
 api.get('/following/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowingUsers);
+api.get('/followed/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowedUser);
 
 module.exports = api;
