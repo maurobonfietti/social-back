43134401d45309cf9ae91ec1d160c5664d96205f
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-04-02 15:22:32 -0300

    Get Users function with async.

--- controllers/user.js
+++ controllers/user.js
@@ -125,24 +125,64 @@ async function followThisUser(identity_user_id, user_id) {
 }
 
 function getUsers(req, res) {
-//    var identity_user_id = req.user.sub;
-    var page = 1;
+    var identity_user_id = req.user.sub;
 
+    var page = 1;
     if (req.params.page) {
         page = req.params.page;
     }
 
     var itemsPerPage = 5;
+
     User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) => {
         if (!users) return res.status(404).send({message: "Users Not Found."});
         if (err) return res.status(500).send({message: "Request Error."});
 
-        return res.status(200).send({
-            users,
-            total,
-            pages: Math.ceil(total/itemsPerPage)
+        followUserIds(identity_user_id).then((value) => {
+            return res.status(200).send({
+                users,
+                user_following: value.following,
+                user_follow_me: value.followed,
+                total,
+                pages: Math.ceil(total/itemsPerPage)
+            });
         });
     });
+}
+
+async function followUserIds(user_id) {
+    var following = await Follow.find({"user": user_id}).select({'_id': 0, '__v': 0, 'user': 0}).exec()
+        .then((following) => {
+            return following;
+        })
+        .catch((err) => {
+            return handleError(err);
+        });
+
+    var followed = await Follow.find({"followed": user_id}).select({'_id': 0, '__v': 0, 'followed': 0}).exec()
+        .then((followed) => {
+            return followed;
+        })
+        .catch((err) => {
+            return handleError(err);
+        });
+
+    var following_clean = [];
+
+    following.forEach((follow) => {
+        following_clean.push(follow.followed);
+    });
+
+    var followed_clean = [];
+
+    followed.forEach((follow) => {
+        followed_clean.push(follow.user);
+    });
+
+    return {
+        following: following_clean,
+        followed: followed_clean
+    };
 }
 
 function updateUser(req, res) {
