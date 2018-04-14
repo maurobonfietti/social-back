e3f3752f1f208e7451f7297ada241d59d771d44b
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-04-14 16:21:11 -0300

    New endpoint for get publications.

--- controllers/publication.js
+++ controllers/publication.js
@@ -23,7 +23,7 @@ function savePublication(req, res) {
     publication.file = 'null';
     publication.user = req.user.sub;
     publication.created_at = moment().unix();
-    
+
     publication.save((err, publicationStored) => {
         if (err) return res.status(500).send({message: "Saving publication error."});
         if (!publicationStored) return res.status(404).send({message: "Publication not saved."});
@@ -32,7 +32,38 @@ function savePublication(req, res) {
     });
 }
 
+function getPublications(req, res) {
+    var page = 1;
+    if (req.params.page) {
+        page = req.params.page;
+    }
+
+    var itemsPerPage = 4;
+    Follow.find({user: req.user.sub}).populate('followed').exec((err, follows) => {
+        if (err) return res.status(500).send({message: "Get publications error."});
+
+        var follows_clean = [];
+
+        follows.forEach((follow) => {
+            follows_clean.push(follow.followed);
+        });
+
+        Publication.find({user: {"$in": follows_clean}}).sort('-created_at').populate('user').paginate(page, itemsPerPage, (err, publications, total) => {
+            if (err) return res.status(500).send({message: "Get publications error..."});
+            if (!publications) return res.status(404).send({message: "Publications not found."});
+
+            return res.status(200).send({
+                total_items: total,
+                pages: Math.ceil(total/itemsPerPage),
+                page: page,
+                publications
+            });
+        });
+    });
+}
+
 module.exports = {
     test,
-    savePublication
+    savePublication,
+    getPublications
 };
