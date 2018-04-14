178983dddc27d731b76669d33c280eb6b42b8519
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-04-14 15:15:40 -0300

    Added field created at, in publication model.

--- models/publication.js
+++ models/publication.js
@@ -6,6 +6,7 @@ var Schema = mongoose.Schema;
 var PublicationSchema = Schema({
     text: String,
     file: String,
+    created_at: String,
     user: { type: Schema.ObjectId, ref: 'User' }
 });
 
