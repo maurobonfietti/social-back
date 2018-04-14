11d7651c259d1753e173b760e8685c91b427b35c
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-04-14 15:09:42 -0300

    New endpoint for save a publication.

--- controllers/publication.js
+++ controllers/publication.js
@@ -13,6 +13,26 @@ function test(req, res) {
     return res.status(200).send({message: "Publication Controller..."});
 }
 
+function savePublication(req, res) {
+    var params = req.body;
+
+    if (!params.text) return res.status(200).send({message: "Text field is required."});
+
+    var publication = new Publication();
+    publication.text = params.text;
+    publication.file = 'null';
+    publication.user = req.user.sub;
+    publication.created_at = moment().unix();
+    
+    publication.save((err, publicationStored) => {
+        if (err) return res.status(500).send({message: "Saving publication error."});
+        if (!publicationStored) return res.status(404).send({message: "Publication not saved."});
+
+        return res.status(200).send({publication: publicationStored});
+    });
+}
+
 module.exports = {
-    test
+    test,
+    savePublication
 };
--- routes/publication.js
+++ routes/publication.js
@@ -5,6 +5,10 @@ var PublicationController = require('../controllers/publication');
 var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
-api.get('/publication', md_auth.ensureAuth, PublicationController.test);
+var multipart = require('connect-multiparty');
+var md_upload = multipart({uploadDir: './uploads/publications'});
+
+api.get('/publication-test', md_auth.ensureAuth, PublicationController.test);
+api.post('/publication', md_auth.ensureAuth, PublicationController.savePublication);
 
 module.exports = api;
