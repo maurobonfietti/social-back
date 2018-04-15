ea5d2f4ba8b083fb0dc6f841a288fc4036431577
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-04-15 17:04:28 -0300

    Minor changes.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ mongoose.connect('mongodb://localhost:27017/social')
         .then(() => {
             console.log('DB: Connect OK!');
             app.listen(port, () => {
-                console.log('Server running on => http://localhost:3800');
+                console.log('Server running on => http://localhost:' + port);
             });
         })
         .catch(err => console.log(err));
