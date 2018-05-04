20f57ea544366065edc9d880091c8f0c7ab2e079
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-05-04 19:53:04 -0300

    Minor changes in get received messages endpoint.

--- controllers/message.js
+++ controllers/message.js
@@ -42,7 +42,7 @@ function getReceivedMessages(req, res) {
 
     var itemsPerPage = 4;
 
-    Message.find({receiver: userId}).populate('emitter').paginate(page, itemsPerPage, (err, messages, total) => {
+    Message.find({receiver: userId}).populate('emitter', 'name surname nick image _id').paginate(page, itemsPerPage, (err, messages, total) => {
         if (err) return res.status(500).send({message: 'Get message error...'});
         if (!messages) return res.status(404).send({message: 'No messages...'});
 
