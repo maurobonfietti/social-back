a8384fdcea8ec9effdfffeb7a4a90753305d016a
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-05-04 20:27:27 -0300

    Configure headers and CORS access in NodeJS.

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ var message_routes = require('./routes/message');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+ 
+    next();
+});
+
 app.use('/api', user_routes);
 app.use('/api', follow_routes);
 app.use('/api', publication_routes);
