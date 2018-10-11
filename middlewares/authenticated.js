b3cab4062646693387365299c0899037cd3647a4
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-10-11 19:42:15 -0300

    Cleaning and remove console.log

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -1,28 +1,22 @@
 'use strict';
 
 var jwt = require('jwt-simple');
-var moment = require('moment');
+//var moment = require('moment');
 var secret = 'Secret_Key1-2-3.';
 
 exports.ensureAuth = function(req, res, next) {
     if (!req.headers.authorization) {
         return res.status(403).send({message: 'Forbidden: Invalid Token.'});
     }
-
     var token = req.headers.authorization.replace(/['"]+/g, '');
-    console.log(token);
-
     try  {
         var payload = jwt.decode(token, secret);
 //        if (payload.expired < moment().unit()) {
 //            return res.status(401).send({message: 'Expired Token.'});
 //        }
     } catch (ex) {
-//        console.log('catch...');
         return res.status(403).send({message: 'Forbidden: Invalid Token...'});
     }
-
     req.user = payload;
-
     next();
 };
