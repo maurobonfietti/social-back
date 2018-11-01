9610f86400cff807906369e46a35e1624c83b000
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-11-01 15:19:43 -0300

    followUserIds.

--- controllers/follow.js
+++ controllers/follow.js
@@ -51,10 +51,22 @@ function getFollowingUsers(req, res) {
         if (err) return res.status(500).send({message: "Get follow error."});
         if (!follows) return res.status(404).send({message: "Without follows."});
 
-        return res.status(200).send({
-            total: total,
-            pages: Math.ceil(total/itemsPerPage),
-            follows: follows
+//        return res.status(200).send({
+//            total: total,
+//            pages: Math.ceil(total/itemsPerPage),
+//            follows: follows
+//        });
+        
+        followUserIds(req.user.sub).then((value) => {
+            return res.status(200).send({
+                x1: "x1t1",
+                total: total,
+                pages: Math.ceil(total/itemsPerPage),
+//                follows: follows,
+                follows,
+                user_following: value.following,
+                user_follow_me: value.followed,
+            });
         });
     });
 }
@@ -100,6 +112,41 @@ function getMyFollows(req, res) {
 
         return res.status(200).send({follows});
     });
+}
+
+async function followUserIds(user_id) {
+    var following = await Follow.find({"user": user_id}).select({'_id': 0, '__v': 0, 'user': 0}).exec()
+        .then((following) => {
+            return following;
+        })
+        .catch((err) => {
+            return handleError(err);
+        });
+
+    var followed = await Follow.find({"followed": user_id}).select({'_id': 0, '__v': 0, 'followed': 0}).exec()
+        .then((followed) => {
+            return followed;
+        })
+        .catch((err) => {
+            return handleError(err);
+        });
+
+    var following_clean = [];
+
+    following.forEach((follow) => {
+        following_clean.push(follow.followed);
+    });
+
+    var followed_clean = [];
+
+    followed.forEach((follow) => {
+        followed_clean.push(follow.user);
+    });
+
+    return {
+        following: following_clean,
+        followed: followed_clean
+    };
 }
 
 module.exports = {
