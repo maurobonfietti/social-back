b9577ac1d6bf20b6295fcf1345cae38538d9bbc7
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-12-03 23:24:29 -0300

    Validate Expired Token.

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -1,22 +1,23 @@
 'use strict';
 
 var jwt = require('jwt-simple');
-//var moment = require('moment');
+var moment = require('moment');
 var secret = 'Secret_Key1-2-3.';
 
-exports.ensureAuth = function(req, res, next) {
+exports.ensureAuth = function (req, res, next) {
     if (!req.headers.authorization) {
         return res.status(403).send({message: 'Forbidden: Invalid Token.'});
     }
     var token = req.headers.authorization.replace(/['"]+/g, '');
-    try  {
+    try {
         var payload = jwt.decode(token, secret);
-//        if (payload.expired < moment().unit()) {
-//            return res.status(401).send({message: 'Expired Token.'});
-//        }
+        if (payload.exp <= moment().unix()) {
+            return res.status(401).send({message: 'Expired Token.'});
+        }
     } catch (ex) {
         return res.status(403).send({message: 'Forbidden: Invalid Token...'});
     }
     req.user = payload;
+
     next();
 };
