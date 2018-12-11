f1bea259967c11e4cc09f193eabab2f1dee14512
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-12-11 00:52:26 -0300

    Fix when check expired token.

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -11,7 +11,7 @@ exports.ensureAuth = function (req, res, next) {
     var token = req.headers.authorization.replace(/['"]+/g, '');
     try {
         var payload = jwt.decode(token, secret);
-        if (payload.exp <= moment().unix()) {
+        if (payload.expired <= moment().unix()) {
             return res.status(401).send({message: 'Expired Token.'});
         }
     } catch (ex) {
