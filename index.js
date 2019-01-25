f919ce337ebc291c33d26e39c373dfed27dd9e37
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2019-01-24 21:50:09 -0300

    Deploy on Heroku.

--- index.js
+++ index.js
@@ -2,20 +2,19 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-//var port = 3800;
 const port = process.env.PORT || 3800;
 
-//mongoose.connect('mongodb://nodejs_api:nodejs_api@ds253889.mlab.com:53889/nodejs_api', { useNewUrlParser: true })
-//        .then(() => {
-//            console.log('DB: Connect OK!');
-//            app.listen(port, () => {
-//                console.log('Server running on => http://localhost:' + port);
-//            });
-//        })
-//        .catch(err => console.log(err));
-//
-//console.log('Starting...');
+mongoose.connect('mongodb://nodejs_api:nodejs_api@ds253889.mlab.com:53889/nodejs_api', { useNewUrlParser: true })
+        .then(() => {
+            console.log('DB: Connect OK!');
+            app.listen(port, () => {
+                console.log('Server running on => http://localhost:' + port);
+            });
+        })
+        .catch(err => console.log(err));
 
-app.listen(port, () => {
-    console.log('Server running on => http://localhost:' + port);
-});
+console.log('Starting...');
+
+//app.listen(port, () => {
+//    console.log('Server running on => http://localhost:' + port);
+//});
