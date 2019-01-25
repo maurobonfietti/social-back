8aa4ccd69b5ea25705d07a05237e0fdc9e10834b
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2019-01-24 22:41:07 -0300

    Using a test database from mLab.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var app = require('./app');
 const port = process.env.PORT || 3800;
 
-mongoose.connect('mongodb://nodejs_api:nodejs_api@ds253889.mlab.com:53889/nodejs_api', { useNewUrlParser: true })
+mongoose.connect('mongodb://social:social1@ds149059.mlab.com:49059/social', { useNewUrlParser: true })
         .then(() => {
             console.log('DB: Connect OK!');
             app.listen(port, () => {
@@ -14,7 +14,3 @@ mongoose.connect('mongodb://nodejs_api:nodejs_api@ds253889.mlab.com:53889/nodejs
         .catch(err => console.log(err));
 
 console.log('Starting...');
-
-//app.listen(port, () => {
-//    console.log('Server running on => http://localhost:' + port);
-//});
