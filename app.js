373cfc485a69f6ddbcbbed044858a7bcd2bc9a20
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2019-04-14 15:18:32 -0300

    New default endpoint to get info about the API.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var default_routes = require('./routes/default');
 var user_routes = require('./routes/user');
 var follow_routes = require('./routes/follow');
 var message_routes = require('./routes/message');
@@ -19,9 +20,10 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use('/', default_routes);
 app.use('/api', user_routes);
 app.use('/api', follow_routes);
-app.use('/api', publication_routes);
 app.use('/api', message_routes);
+app.use('/api', publication_routes);
 
 module.exports = app;
--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,6 @@ var md_auth = require('../middlewares/authenticated');
 var multipart = require('connect-multiparty');
 var md_upload = multipart({uploadDir: './uploads/users'});
 
-api.get('/home', UserController.home);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 api.get('/user/:id', md_auth.ensureAuth, UserController.getUser);
