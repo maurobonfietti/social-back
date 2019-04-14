b59ff1eaab2a6bcfe168fcaa0deb0c2dcb6c5fd4
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2019-04-14 15:26:14 -0300

    Add new endpoint "/status" to check status about the api.

--- routes/default.js
+++ routes/default.js
@@ -5,5 +5,6 @@ var api = express.Router();
 var DefaultController = require('../controllers/default');
 
 api.get('/', DefaultController.help);
+api.get('/status', DefaultController.status);
 
 module.exports = api;
